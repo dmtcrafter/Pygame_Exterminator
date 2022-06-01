46c74bfb0509c7f1074a186a0ff2b95f6d271443
dmtcr <dmtcrafter@gmail.com>
2022-06-01 14:04:54 -0600

    6/1/2022 2:04 DMT

--- game.py
+++ game.py
@@ -21,6 +21,34 @@ pygame.display.set_caption('Py_Exterminator')
 pygame.display.set_icon(pygame.image.load('./assets/images/icon.png'))
 background = pygame.image.load('./assets/images/background_1.png')
 
+
+class Player(pygame.sprite.Sprite):
+
+    surf = pygame.image.load('./assets/images/player_bot_1.png')
+    rect = surf.get_rect()
+    rect.center = (200, 640)
+    gravity = 0
+
+    def __init__(self):
+        super(Player, self).__init__()
+
+    def update(self):
+
+        if Player.rect.bottomy >= 640:
+            Player.rect.bottomy = 640
+
+        if Player.rect.right >= 1200:
+            Player.rect.right = 1200
+
+        if Player.rect.left >= 0:
+            Player.rect.left = 0
+
+
+sprites = pygame.sprite.Group()
+
+player = Player()
+sprites.add(player)
+
 running = True
 while running:
 
@@ -30,14 +58,17 @@ while running:
                 running = False
                 exit()
 
-            elif event.type == QUIT:
-                running = False
-                exit()
+        elif event.type == QUIT:
+            running = False
+            exit()
+
+    player.gravity += 1
+
+    player.rect.move_ip(0, player.gravity)
 
     screen.blit(background, (0, 0))
 
-    turret = pygame.image.load('./assets/images/turret_open.png')
-    screen.blit(turret, (600, 640))
+    screen.blit(Player.surf, Player.rect.center)
 
     pygame.display.flip()
 
